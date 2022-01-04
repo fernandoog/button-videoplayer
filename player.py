221e16e8e55271efdd8ed0ed0a6212c62d01d929
Fernando Ortega <fernandoog@gmail.com>
2022-01-04 21:57:50 +0100

    player

--- player.py
+++ player.py
@@ -7,11 +7,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 player = mpv.MPV(ytdl=True, input_default_bindings=True, input_vo_keyboard=True)
 player.fullscreen = True
-player.play('init.mp4')
 
 while True:     
+
     GPIO.wait_for_edge(4, GPIO.RISING)
-    print('pressed')
     start = time.time()
     time.sleep(0.2)
     
@@ -20,9 +19,6 @@ while True:
     lenght = time.time() - start
     
     if lenght > 5:
-        print('long press, exit')
         player.play('init.mp4')
     else:
-        print('short press, play video')
-        player['loop'] = 'inf'
         player.play('test.webm')
