6d03100b6b3731dcb209cdb50f2163cf46046923
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:48:06 +0100

    remove parameters

--- src/player.py
+++ src/player.py
@@ -9,12 +9,12 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 os.system("clear")
 
 while True:  # Run forever
-    time.sleep(2)
+
     if GPIO.input(10) == GPIO.HIGH:
-        player = mpv.MPV(ytdl=True)
+        player = mpv.MPV()
         player.play('test.webm')
         player.wait_for_playback()
         del player
-        os.system("clear")
+        time.sleep(2)
 
 
