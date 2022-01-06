627ee760dc4d28752b13015bcf33b0b7e81c67c1
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:45:09 +0100

    time

--- src/player.py
+++ src/player.py
@@ -1,6 +1,7 @@
 import mpv
 import RPi.GPIO as GPIO
 import os
+import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -8,10 +9,12 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 os.system("clear")
 
 while True:  # Run forever
+    time.sleep(2)
     if GPIO.input(10) == GPIO.HIGH:
         player = mpv.MPV(ytdl=True)
         player.play('test.webm')
         player.wait_for_playback()
         del player
+        os.system("clear")
 
 
