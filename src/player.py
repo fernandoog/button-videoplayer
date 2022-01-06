4c2fcf66c13ee815cf92173ba80832e0bd337cde
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:42:38 +0100

    del

--- src/player.py
+++ src/player.py
@@ -5,12 +5,13 @@ import os
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-player = mpv.MPV(ytdl=True)
 os.system("clear")
 
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:
+        player = mpv.MPV(ytdl=True)
         player.play('test.webm')
         player.wait_for_playback()
+        del player
 
 
