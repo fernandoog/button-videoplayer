bb1743c1e9ae3c007d0f639305f567fecf43c2e3
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 12:41:06 +0100

    test

--- ejemplos/ledcontrol.py
+++ ejemplos/ledcontrol.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 17
+LED_PIN = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.output(LED_PIN, GPIO.HIGH)
--- src/player.py
+++ src/player.py
@@ -1,25 +1,6 @@
 import mpv
-import RPi.GPIO as GPIO
-import time
 
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 player = mpv.MPV(ytdl=True, input_default_bindings=True, input_vo_keyboard=True)
 player.fullscreen = True
 player.play('init.mp4')
 
-while True:     
-
-    GPIO.wait_for_edge(4, GPIO.RISING)
-    start = time.time()
-    time.sleep(0.2)
-    
-    while GPIO.input(4) == GPIO.HIGH:
-        time.sleep(0.1)
-    lenght = time.time() - start
-    
-    if lenght > 5:
-        player.play('init.mp4')
-    else:
-        player.play('test.webm')
