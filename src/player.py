113afe2a259fdb3b0ebeedab55df979dedf4f2b4
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:28:12 +0100

    clear

--- src/player.py
+++ src/player.py
@@ -6,6 +6,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 player = mpv.MPV(ytdl=True)
+os.system("clear")
 
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:
