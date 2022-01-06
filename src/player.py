12a181be90f1aa78e1f4150bf6e6373089eb8d34
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 14:01:54 +0100

    tk

--- src/player.py
+++ src/player.py
@@ -2,6 +2,7 @@ import mpv
 import RPi.GPIO as GPIO
 import os
 import time
+import tkinter as tk
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -9,8 +10,9 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 os.system("clear")
 
 while True:
+
     GPIO.wait_for_edge(10, GPIO.RISING)
-    print('pressed')
+    print('play')
     start = time.time()
     time.sleep(0.2)
 
@@ -21,9 +23,12 @@ while True:
     if lenght > 5:
         os.system("sudo reboot")
     else:
+        root = tk.Tk()
         player = mpv.MPV()
         player.play('video.mp4')
         player.wait_for_playback()
         del player
-        time.sleep(2)
+        tk.mainloop()
+        os.system("clear")
+
 
