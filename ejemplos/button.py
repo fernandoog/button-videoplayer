be033c53ee65cc0f0275e5f1c7ae4d6a2fff46e5
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:03:07 +0100

    button

--- ejemplos/button.py
+++ ejemplos/button.py
@@ -5,5 +5,5 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN,
            pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 while True:  # Run forever
-    if GPIO.input(4) == GPIO.HIGH:
+    if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
