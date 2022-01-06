5019fed0cd32f941fd8fb0c630cb5a1eafb0400c
Fernando Ortega <fortegag.pex@sanitas.es>
2022-01-06 13:40:23 +0100

    wait

--- src/player.py
+++ src/player.py
@@ -1,14 +1,12 @@
 import mpv
 import RPi.GPIO as GPIO
 import os
-import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 player = mpv.MPV(ytdl=True)
 os.system("clear")
-player.wait_for_playback()
 
 while True:  # Run forever
     if GPIO.input(10) == GPIO.HIGH:
